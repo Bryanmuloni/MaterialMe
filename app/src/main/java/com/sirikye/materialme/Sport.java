7650ab8bdbcf6cbe4582d85d574ce3e06958cbc0
Sirikye Brian <bryanmuloni@gmail.com>
2020-01-22 08:59:56 +0300

    MaterialMe AAD codelab, initial commit

---
+++ app/src/main/java/com/sirikye/materialme/Sport.java
@@ -0,0 +1,51 @@
+package com.sirikye.materialme;
+
+/**
+ * Created by Sirikye Brian on 1/21/2020.
+ * bryanmuloni@gmail.com
+ */
+/**
+ * Data model for each row of the RecyclerView
+ */
+class Sport {
+
+    // Member variables representing the title and information about the sport.
+    private String title;
+    private String info;
+
+    private final int imageResource;
+
+    /**
+     * Constructor for the Sport data model.
+     *
+     * @param title The name if the sport.
+     * @param info Information about the sport.
+     */
+    Sport(String title, String info, int image) {
+        this.title = title;
+        this.info = info;
+        this.imageResource = image;
+    }
+
+    /**
+     * Gets the title of the sport.
+     *
+     * @return The title of the sport.
+     */
+    String getTitle() {
+        return title;
+    }
+
+    /**
+     * Gets the info about the sport.
+     *
+     * @return The info about the sport.
+     */
+    String getInfo() {
+        return info;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+}
